121c191c20f3e72700250f4fa6d9b3773cb72d9d
Daniel Brum <dani3544@gmail.com>
2018-05-10 22:09:37 +0000

    Update cachesim.cpp

--- CacheSim/cachesim.cpp
+++ CacheSim/cachesim.cpp
@@ -2,6 +2,22 @@
 #include <vector>
 using namespace std;
 
-class LRU {
+class Cache {
+    public:
+    int misses;
+    int access;
+
+    vector<conjassoc> int;
+};
+
+class Address {
+    int val; //int 32 bits
+
+    //Calcular index fazendo shift
+
+    //Calcular tag fazendo shift
+};
+
+class ConjuntoAssoc {
     
 };
